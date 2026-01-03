0b424b1bc666783868d83c5e8412a6d0022ec6dd
Himanshu <154817641+himans533@users.noreply.github.com>
2026-01-03 14:21:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -288,8 +288,8 @@ def get_db_connection():
     return psycopg2.connect(
         host=os.getenv("PGHOST"),
         database=os.getenv("POSTGRES_DB"),
-        user=os.getenv("POSTGRES_USER"),
-        password=os.getenv("POSTGRES_PASSWORD"),
+        user=os.getenv("PGUSER"),
+        password=os.getenv("PGPASSWORD"),
         port=os.getenv("PGPORT"),
         cursor_factory=RealDictCursor
     )
